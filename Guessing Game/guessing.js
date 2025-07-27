23790e7cf5df73b1d5a2c79a6f4b6740fdd8ca38
Neeraj <neerajsg.gupta@gmail.com>
2025-07-27 20:18:42 +0530

    initial commit

---
+++ Guessing Game/guessing.js
@@ -0,0 +1,21 @@
+let max = prompt("Enter the maximum number for the guessing game:");
+let randomnumber=Math.floor(Math.random() * max) + 1;
+console.log(randomnumber);
+let guess = prompt("guess the number");
+while(true){
+
+if(guess=="quit"){
+    console.log("user quit the game");
+    break;
+}
+if(guess==randomnumber){
+    console.log("Congratulations! You guessed the number correctly.",ramdomnumber);
+    break;
+}
+else if(guess<randomnumber){
+  guess= prompt("your guess is too low");
+}
+else{
+    guess=prompt("your guess is too high");
+}
+}
